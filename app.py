bef7c208a643818352ef4a644eb1d7510797ddcc
Ash515 <aaashwin515@gmail.com>
2021-05-06 12:13:58 +0530

    updated new files

--- app.py
+++ app.py
@@ -24,9 +24,45 @@ def index():
 
 @app.route('/profile',methods=['POST','GET'])
 def profile():
+    if request.method=="POST":
+        username=request.form['u_name']
+        userage=request.form['u_age']
+        usermail=request.form['u_email']
+        userphno=request.form['u_phno']
+        useraddress=request.form['u_address']
+        cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO userprofile VALUES(%s,%s,%s,%s,%s)',(usermail,username,userage,userphno,useraddress))
+        cursor.connection.commit()
+        return redirect(url_for('symptoms'))
+    return render_template('profile.html')
     
 
-    return render_template('profile.html')
+   
+
+@app.route('/symptoms',methods=['POST','GET'])
+def symptoms():
+     if request.method=="POST":
+        usermail=request.form['u_mail']
+        userphno=request.form['u_phno']
+        fever=request.form['fever']
+        bodypain=request.form['bodypain']
+        lowappetite=request.form['lowappetite']
+        musclepain=request.form['musclepain']
+        cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO usersymptoms VALUES(%s,%s,%s,%s,%s,%s)',(usermail,userphno,fever,bodypain,lowappetite,musclepain,))
+        cursor.connection.commit()
+        return redirect(url_for('index'))
+     return render_template('symptoms.html')
+
+@app.route('/reports')
+def reports():
+    cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT mail,phno,status FROM results')
+    cursor.connection.commit()
+    reportdata=cursor.fetchall()
+    return render_template('reports.html',reportdata=reportdata)    
+
+    
 
 
 if __name__=='__main__':
